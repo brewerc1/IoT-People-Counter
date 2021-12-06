522605a8bc3c6a147389749d1f9275f827758dc5
Jonathan Makunga <jmakunga2015@gmail.com>
2021-12-06 17:59:18 -0500

    Lambda Handler setup

---
+++ AWSIoTLambda/IoTLambdaHandler/src/main/java/AWSIoTLambda/IoTLambdaHandler/IoTLambdaHandlerApplication.java
@@ -0,0 +1,13 @@
+package AWSIoTLambda.IoTLambdaHandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTLambdaHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTLambdaHandlerApplication.class, args);
+	}
+
+}
---
+++ AWSIoTLambda/IoTLambdaHandler/src/test/java/AWSIoTLambda/IoTLambdaHandler/IoTLambdaHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package AWSIoTLambda.IoTLambdaHandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTLambdaHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
