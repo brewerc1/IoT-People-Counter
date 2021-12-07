cbf6159393127a5f6efb4efab6688d91927495c4
Jonathan Makunga <jmakunga2015@gmail.com>
2021-12-07 00:34:37 -0500

    Added shell scripts

--- AWSIoTLambda/IoTLambda/deploy.sh
+++ AWSIoTLambda/IoTLambda/deploy.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
-#
+FILE=aws_account.txt
+
+if [ ! -f "$FILE" ]; then
+  echo "aws_account.txt is needed"
+  echo "Please, run generate_aws_account_file.sh to create it."
+fi
+
 npx cdk synth
-
 #
-cdk bootstrap
-
+# Bootstrapping
+input="aws_account.txt"
+while IFS= read -r line
+do
+  cdk bootstrap aws://"$line"
+done < "$input"
 #
+##
 npx cdk deploy
--- AWSIoTLambda/IoTLambda/lib/io_t_lambda-stack.ts
+++ AWSIoTLambda/IoTLambda/lib/io_t_lambda-stack.ts
@@ -9,7 +9,7 @@ import {Queue} from "aws-cdk-lib/aws-sqs";
 
 export class IoTLambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    super(scope, `${id}-1`, props);
 
     // The code that defines your stack goes here
     const lambdaFunction = new lambda.Function(this, 'IoTLambdaFunction', {
